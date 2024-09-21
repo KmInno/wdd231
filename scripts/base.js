e59c41381735c427eba33f1c2bf2d4e1026c7cd6
KmInno <innocentmuwanda0@gmail.com>
2024-09-21 18:54:32 +0300

    windows

---
+++ scripts/base.js
@@ -0,0 +1,9 @@
+function displayDateModified() {
+    const lastModified = new Date(document.lastModified);
+    const lastModifiedSpan = document.getElementById('displayDateModified');
+    lastModifiedSpan.textContent = lastModified.toLocaleString();
+}
+
+window.onload = function() {
+    displayDateModified();
+}
